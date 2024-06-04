5e1f23506b27044b83244d97c961bb661523d3dc
DharmikGohil013 <dharmikgohil395003@gmail.com>
2024-06-04 10:30:46 +0530

    add Time.deltatime keyword

--- Assets/SCRIPTS/PLAYERCONTROLAER.cs
+++ Assets/SCRIPTS/PLAYERCONTROLAER.cs
@@ -13,6 +13,6 @@ public class PLAYERCONTROLAER : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector3.forward *Time.deltaTime);
     }
 }
